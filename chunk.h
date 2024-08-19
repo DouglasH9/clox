d223f8132e1f04f314327cf31e14137d0531a713
DouglasH9 <DouglasH9882@gmail.com>
2024-08-19 14:43:30 -0500

    adds line tracking and values

--- chunk.h
+++ chunk.h
@@ -6,8 +6,10 @@
 #define LOX_CHUNK_H
 
 #include "common.h"
+#include "value.h"
 
 typedef enum {
+    OP_CONSTANT,
     OP_RETURN,
 } OpCode;
 
@@ -15,10 +17,13 @@ typedef struct {
     int count;
     int capacity;
     uint8_t* code;
+    int* lines;
+    ValueArray constants;
 } Chunk;
 
 void init_chunk(Chunk* chunk);
 void free_chunk(Chunk* chunk);
-void write_chunk(Chunk* chunk, uint8_t byte);
+void write_chunk(Chunk* chunk, uint8_t byte, int line);
+int add_constant(Chunk* chunk, Value value);
 
 #endif //LOX_CHUNK_H
